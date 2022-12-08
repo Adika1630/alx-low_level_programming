bf677f4a7e81c9be1690783155ddbd6d813e825f
Adika1630 <addisu05mulat@gmail.com>
2022-12-08 06:07:36 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-int main() {
-	char Mystr[] = ""Programming is like building a multilingual puzzle";
-	pits(Mystr);    // writing the string to stdout
-
-	return(0);
+#include <stdio.h>
+/**
+ *  Description: main - Prints out a string to stdout.
+ *  Return: 0 if success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
