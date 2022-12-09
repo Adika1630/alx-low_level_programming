0be757be176f0aa49c8e33d1588dc9552b354dfa
Adika1630 <addisu05mulat@gmail.com>
2022-12-09 03:49:20 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("negative\n");
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("zero");
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
