8c41c5a4a44b3aeb486ad60f72015dbfc7543347
Adika1630 <addisu05mulat@gmail.com>
2022-12-15 08:17:01 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagobal lines of a given number
+ * @n: number of times character
+ *
+ * Return: 0 Always (success)
+ */
+void print_diagonal(int n)
+{
+	int co, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			for (sp = 1; sp <= co; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
