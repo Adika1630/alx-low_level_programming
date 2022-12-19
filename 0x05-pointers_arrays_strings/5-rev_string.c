4bbef22abfa2ff9adbc3bc61d915cd63e24d330c
Adika1630 <addisu05mulat@gmail.com>
2022-12-19 00:14:12 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - functions tha reverse a string
+ * @s: String reverse
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int aux = 0;
+	char ltemp;
+
+	while (*(s + i) != '\0')
+		i = i + 1;
+	i = i - 1;
+	while (aux < i)
+	{
+		ltemp = s[i];
+		s[i] = s[aux];
+		s[aux] = ltemp;
+		aux++;
+		i--;
+	}
+}
